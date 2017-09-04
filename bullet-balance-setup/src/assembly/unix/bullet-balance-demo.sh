9c9efa7468d58dde8aa5a726b1a7ade1972188dc
Anna Berdonosova <berdonosova@devexperts.com>
2017-09-04 10:22:08 +0300

    Correct unix demo script

--- bullet-balance-setup/src/assembly/unix/bullet-balance-demo.sh
+++ bullet-balance-setup/src/assembly/unix/bullet-balance-demo.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 if [ -z $APP_HOME ]; then
-	APP_HOME=`dirname $0`
+        APP_HOME=`dirname $0`
 fi
 
 LIB=${APP_HOME}/lib
-CONFIG=${APP_HOME}/configuration
 
 if [ -z $JAVA_HOME ]; then
-	JAVA=java
-else 
-	JAVA=$JAVA_HOME/bin/java
+        JAVA=java
+else
+        JAVA=$JAVA_HOME/bin/java
 fi
 
 CLASSPATH=$(find "$LIB" -name '*.jar' |xargs echo  |tr ' ' ':')
