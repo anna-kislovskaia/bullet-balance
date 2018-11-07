ff275a43241d0399cf8b7e750b9b56ef304bcab1
Anna Kislovskaia <berdonosova@devexperts.com>
2018-11-07 13:54:45 +0300

    Update weights calculator

--- bullet-balance-analytics/src/main/java/com/bulletbalance/random/NoShortSellWeightsGenerator.java
+++ bullet-balance-analytics/src/main/java/com/bulletbalance/random/NoShortSellWeightsGenerator.java
@@ -5,45 +5,43 @@ import com.bulletbalance.utils.MathUtils;
 import javax.annotation.Nonnull;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Generates non-negative weights of all assets
  */
 public class NoShortSellWeightsGenerator implements RandomWeightsGenerator {
+	private static final BigDecimal TOTAL = BigDecimal.ONE.setScale(MathUtils.DEFAULT_PRECISION, RoundingMode.HALF_UP);
 
 	@Nonnull
 	public BigDecimal[] generateWeights(int count) {
-	 if (count == 1) {
-		 return new BigDecimal[]{BigDecimal.ONE};
-	 }
-	 List<BigDecimal> weights = new ArrayList<>(count);
-	 ThreadLocalRandom random = ThreadLocalRandom.current();
-	 BigDecimal restOfTotal = BigDecimal.ONE;
-	 for (int i = 0; i < count; i++) {
-		 if (restOfTotal.doubleValue() == 0) {
-			 weights.add(BigDecimal.ZERO);
-			 continue;
-		 }
-		 if (i == count - 1) {
-			 weights.add(restOfTotal);
-			 break;
-		 }
-		 double rawWeight = random.nextDouble(1 + MathUtils.EPS);
-		 BigDecimal precise = new BigDecimal(rawWeight).setScale(MathUtils.DEFAULT_PRECISION, RoundingMode.HALF_UP);
-		 if (restOfTotal.compareTo(precise) > 0) {
-			 restOfTotal = restOfTotal.subtract(precise);
-			 weights.add(precise);
-		 } else {
-			 weights.add(restOfTotal);
-			 restOfTotal = BigDecimal.ZERO;
-		 }
-	 }
-	 Collections.shuffle(weights);
-	 return weights.toArray(new BigDecimal[count]);
+		if (count == 1) {
+			return new BigDecimal[]{TOTAL};
+		}
+		double[] randomWeights = new double[count];
+		ThreadLocalRandom random = ThreadLocalRandom.current();
+		double totalWeight = 0;
+		for (int i = 0; i < count; i++) {
+			randomWeights[i] = random.nextInt(101);
+			totalWeight += randomWeights[i];
+		}
+
+		BigDecimal[] weights = new BigDecimal[count];
+		BigDecimal restOfTotal = TOTAL;
+		for (int i = 0; i < count; i++) {
+			BigDecimal weight = new BigDecimal(randomWeights[i] / totalWeight)
+					.setScale(MathUtils.DEFAULT_PRECISION, RoundingMode.HALF_UP);
+			if (restOfTotal.compareTo(weight) > 0 && i < count - 1) {
+                weights[i] = weight;
+                restOfTotal = restOfTotal.subtract(weight);
+			} else if (restOfTotal.compareTo(BigDecimal.ZERO) > 0) {
+				weights[i] = restOfTotal;
+				restOfTotal = BigDecimal.ZERO;
+			} else {
+				weights[i] = BigDecimal.ZERO;
+			}
+		}
+		return weights;
  }
 
 }
