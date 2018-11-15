9aef3da9ce45a4a58c8cb68362a011a2ad0226d3
Anna Kislovskaia <berdonosova@devexperts.com>
2018-11-15 13:43:09 +0300

    Web UI skeleton - hot reload attempt 1

--- bullet-balance-ui/src/index.tsx
+++ bullet-balance-ui/src/index.tsx
@@ -2,7 +2,19 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {AppComponent} from "./modules/app/components/app.component";
 
-ReactDOM.render(
-    <AppComponent compiler="TypeScript" framework="React" />,
-    document.getElementById("root")
-);
+const root =  document.getElementById("root");
+
+const render = (Component: typeof AppComponent) => {
+    const mainComponent = <Component compiler="TypeScript" framework="React" />;
+    ReactDOM.render(mainComponent, root);
+};
+
+render(AppComponent);
+
+if (module.hot) {
+    module.hot.accept('./modules/app/components/app.component', () => {
+        console.log("Reloading Sources...");
+        const nextApp = require('./modules/app/components/app.component').AppComponent; //tslint:disable-line no-require-imports
+        render(nextApp);
+    });
+}
