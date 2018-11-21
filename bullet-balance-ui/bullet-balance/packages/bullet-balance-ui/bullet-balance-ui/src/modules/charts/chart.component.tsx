2fe925bd78adbb5bbdf36db170ff270faf379f5e
Anna Kislovskaia <berdonosova@devexperts.com>
2018-11-21 12:09:37 +0300

    - add legend

--- bullet-balance-ui/bullet-balance/packages/bullet-balance-ui/bullet-balance-ui/src/modules/charts/chart.component.tsx
+++ bullet-balance-ui/bullet-balance/packages/bullet-balance-ui/bullet-balance-ui/src/modules/charts/chart.component.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Component } from 'react';
+import { Component, Fragment } from 'react';
 import { TChartData, TChartSeries } from "./chart.model";
 import { XYChart, XAxis, YAxis, LineSeries } from "@data-ui/xy-chart";
 
@@ -12,27 +12,45 @@ export interface XYChartProps {
 export class XYChartComponent extends Component<XYChartProps, {}> {
     render() {
         const { chartData, children } = this.props;
-        const legend = chartData.series.map(serie => {
-           return {title: serie.name, color: serie.color};
-        });
 
         const xScaleType = chartData.enableTime ? 'time' : 'linear';
         return (
-            <XYChart {...this.props}
-                     xScale={{ type: xScaleType }}
-                     yScale={{ type: 'linear' }}
-                     ariaLabel={'test label'} >
-                <XAxis />
-                <YAxis orientation={'left'}  />
-                {chartData.series.map(entity => this.renderPlot(entity))}
-                {children}
-            </XYChart>
+            <Fragment>
+                <XYChart {...this.props}
+                         xScale={{ type: xScaleType }}
+                         yScale={{ type: 'linear' }}
+                         ariaLabel={'test label'} >
+                    <XAxis />
+                    <YAxis orientation={'left'}  />
+                    {chartData.series.map(entity => this.renderPlot(entity))}
+                    {children}
+                </XYChart>
+                {this.renderLegend(chartData)}
+            </Fragment>
         );
     }
 
     renderPlot = (series: TChartSeries) => {
         return (
             <LineSeries key={series.name} seriesKey={series.name} data={series.points} stroke={series.color}/>
+        );
+    }
+
+    renderLegend = (chartData: TChartData) => {
+        return (
+            <div>
+                {chartData.series.map(serie => {
+                    const style = {stroke: serie.color, strokeWidth : 2};
+                    return (
+                        <div key={`legend-${serie.name}`}>
+                            <svg height="10" width="20">
+                                <line x1="0" y1="5" x2="15" y2="5" style={style} />
+                            </svg>
+                            <span>{serie.name}</span>
+                        </div>
+                    );
+                })}
+            </div>
         );
     }
 }
